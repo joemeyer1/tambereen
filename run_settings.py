32899a4c009443fdcb1bc10703b160082ccb4455
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2026-08-21 11:25:06 -0700

    add a comment
    example of PRETRAINED_MODEL_PATH

--- run_settings.py
+++ run_settings.py
@@ -51,7 +51,8 @@ class PoseEstimatorSettings:
 
 @dataclass
 class AudioMovementProjectorSettings:
-    PRETRAINED_MODEL_PATH: Optional[str] = None  # If you use a pre-trained model, make sure POSE_ESTIMATOR_TYPE aligns
+    PRETRAINED_MODEL_PATH: Optional[str] = None  # this can be set to model run folder (e.g. "output_data_runs/0"); if None then train new model
+    # If you use a pre-trained model, make sure POSE_ESTIMATOR_TYPE aligns
     # If you try using a different pose estimator than the model was trained with, the pose keypoints passed won't align with those the model was trained with and you will get a tensor size mismatch error between actual input and model input dimensions
 
     PYTHON_PLAY_AUDIO: bool = False
